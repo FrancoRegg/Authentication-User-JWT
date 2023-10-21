e446408410486807cdecc8cd2ef11eb480f161f8
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-10-21 18:08:52 +0000

    Cambio en las rutas con los nombres de los componentes

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -25,8 +25,8 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Register />} path="/register" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
