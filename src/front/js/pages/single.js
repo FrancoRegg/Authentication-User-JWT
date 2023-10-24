bdf10e0933879608391b246d828e5c38bbb1bece
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-10-24 15:01:59 +0000

    El componente fue vaciado

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -2,27 +2,20 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+
 
 export const Single = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-			<img src={rigoImageUrl} />
-			<hr className="my-4" />
+  useEffect(() => {
+    if(store.token && store.token != "" && store.token != undefined) actions.getMessage()
+	}, [store.token])
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="test">
+      <div className="alert alert-info">{store.message}</div>
+    </div>
+  );
 };
 
-Single.propTypes = {
-	match: PropTypes.object
-};
+
