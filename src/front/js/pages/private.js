7b11abdda15ef2072a319e261ab75626b2da20ca
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-10-25 08:49:28 +0000

    Cambio nombre del componente

---
+++ src/front/js/pages/private.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+
+export const Private = props => {
+  const { store, actions } = useContext(Context);
+
+  useEffect(() => {
+    if(store.token && store.token != "" && store.token != undefined) actions.getMessage()
+	}, [store.token])
+
+  return (
+    <div className="test">
+      <div className="alert alert-info">{store.message}</div>
+    </div>
+  );
+};
+
+
