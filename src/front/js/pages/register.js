d797ecc04ae73916322617b8bbacecb2544bf925
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-28 10:58:30 +0000

    ambio estructuras del componente y funciones

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -6,63 +6,20 @@ export const Register = () => {
   const [mensaje, setMensaje] = useState('');
 
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setRegister({ ...register, [name]: value, });
-    console.log("Aqui event", event);
-  };
-  
-  //La función handleSubmit se encarga de tomar los datos del formulario de registro y los envia al servidor en formato JSON.
-  const handleSubmit = (event) => {
-    event.preventDefault(); //Previene el comportamiento por defecto del navegador
-
-  //Verifica que los campos "email" y "password" esten completos 
-    if (register.email.trim() === '' || register.password.trim() === '') {      //##### Debo ver como mostras el mensaje al campo vacio 
-      setError('All fields are mandatory!');
-      return;
-    }
-  //Envio los datos del formulario a una tabla a traves de un fetch con metodo "post"  
-    fetch(process.env.BACKEND_URL + "/register", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(register),
-    })
-      .then(resp => resp.json())
-      .then(data => {
-        console.log(data);
-
-        //Limpia los campos una vez creado el usuario exitosamente, da paso a poder seguir creando registros
-        setRegister({
-          email: '', password: ""
-        });
-      });
-    setMensaje('The account was created correctly')      //Aviso de que el usuario fue creado con exito 
-  };
 
   return (
     <div className="container-fluid text-center mt-5">
       <div>
         <h1>Registrate</h1>
       </div>
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form">
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email *</label>
-          <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" value={register.email} onChange={handleChange} />
+          <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
 
-          {error && <div className="alert alert-danger p-1" role="alert" >{error}</div>}
-
-          <label for="exampleInputPassword1" class="form-label">Password * </label>
-          <input type="password" class="form-control" name="password" id="exampleInputPassword1" value={register.password} onChange={handleChange} />
-
-          {error && <div className="alert alert-danger p-1" role="alert">{error}</div>}
+          <input type="password" class="form-control" name="password" id="exampleInputPassword1" />
         </div>
         <button type="submit" class="registrate">Submit</button>
       </form>
-      {mensaje && <div class="alert alert-success p-1" role="alert">
-        {mensaje}
-      </div>}
     </div>
   );
 };
