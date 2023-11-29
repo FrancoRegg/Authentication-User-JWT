82f46e8757b1150a5137c354abf7f2b07a5154a2
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-29 08:44:23 +0000

    Añadoconlumna 'full_name' a la tabla ususario

--- src/api/models.py
+++ src/api/models.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(60), unique=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +16,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
