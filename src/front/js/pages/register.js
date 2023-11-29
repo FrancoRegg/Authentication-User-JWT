d9363b93b16c91ead0c9d4f8be24687b6ad8d2fb
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-29 10:06:36 +0000

    Añado inputs para el registro

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -1,24 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
 
 export const Register = () => {
-  const [register, setRegister] = useState({ email: '', password: '' })
-  const [error, setError] = useState('')
-  const [mensaje, setMensaje] = useState('');
+  const { store, actions } = useContext(Context)
+  const [register, setRegister] = useState({ full_name: '', email: '', password: '' })
+  console.log("REGISTRO", register);
 
+  const handleSubmit = (e) =>{
+    e.preventDefault();
 
+    actions.register(register)
+
+  }
 
   return (
-    <div className="container-fluid text-center mt-5">
+    <div className="container-fluid">
       <div>
         <h1>Registrate</h1>
       </div>
-      <form className="form">
-        <div class="mb-3">
-          <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
-
-          <input type="password" class="form-control" name="password" id="exampleInputPassword1" />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="container-inputs">
+          <input 
+            type="text" 
+            className="form-control" 
+            name="full_name" 
+            id="exampleInputEmail1" 
+            aria-describedby="emailHelp" 
+            placeholder="Nombre completo" 
+            value={register.full_name}
+            onChange={(e)=>setRegister({...register, full_name: e.target.value})}
+          />
+          <input 
+            type="email" 
+            className="form-control" 
+            name="email" 
+            id="exampleInputEmail1" 
+            aria-describedby="emailHelp" 
+            placeholder="Correo electrónico" 
+            value={register.email}
+            onChange={(e)=>setRegister({...register, email:e.target.value})}
+          />
+          <input 
+            type="password" 
+            className="form-control" 
+            name="password" 
+            id="exampleInputPassword1" 
+            placeholder="Contraseña" 
+            value={register.password}
+            onChange={(e)=>setRegister({register, password:e.target.value})}
+          />
         </div>
-        <button type="submit" class="registrate">Submit</button>
+        <button className="registrate">Registrarse</button>
       </form>
     </div>
   );
