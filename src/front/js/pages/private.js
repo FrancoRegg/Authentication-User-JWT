5bb87a68d656b5ebf129e1c802236577a2940c8b
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-29 18:28:00 +0000

    añado html

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
+import "../../styles/private.css"
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 
 export const Private = props => {
   const { store, actions } = useContext(Context);
-  console.log("INFO DE USUARIO",store.datauser);
 
   useEffect(() => {
     actions.dataUser()
@@ -13,7 +13,10 @@ export const Private = props => {
 
   return (
     <div className="info-usuario">
-      <h1>Hola {store.datauser.full_name}</h1>
+      <h1 className="saludo-privado">Hola {store.datauser.full_name},</h1>
+      <p className="bienvenida-privada">Bienvenido a tu pagina privada... En un futuro se añadiremos muchas mas funciones, no te lo pierdas.</p>
+      <br/>
+      <p className="despedida-privada">Que tengas un excelente día!!!</p>
     </div>
   );
 };
