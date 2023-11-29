7a6e962daa43e927fc6084c8790259374afd14bc
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-29 09:42:35 +0000

    Añado columna 'full_name' a la tabla user

--- src/api/models.py
+++ src/api/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(60), unique=False)
+    full_name = db.Column(db.String(60), unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
